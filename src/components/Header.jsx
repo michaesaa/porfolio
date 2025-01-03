e86f008aa4fafd72defe19e54c800271de5ff069
Michael Stiven Saa Motta <michaelsaams20@gmail.com>
2025-01-03 02:15:15 -0500

    estilos para el meu-btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,21 +4,44 @@
  */
 export const Header = () => {
   return (
-   <header clasName="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-r from-zinc-900 via-zinc-900 to-zinc-800">
-  <div className="max-w-screen-2xl w-full mx-auto px-4">
-    <h1>
-      <a 
-      href="/"
-      className="logo"
-      >
-        <img
-        src="/images/logo.svg" 
-        alt="" />
-      </a>
-    </h1>
+    <header clasName="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 
+    to-zinc-900/0">
+      <div className="max-w-screen-2xl w-full mx-auto px-4
+      flex justify-between items-center md:px-6 md:grid
+      md:grid-cols-[1fr,3fr,1fr]">
+        <h1>
+          <a
+            href="/"
+            className="logo"
+          >
+            <img
+              src="/images/logo.svg"
+              width={40}
+              alt="michael saa" />
+          </a>
+        </h1>
 
-  </div>
-   </header>
+        <div className="relative md:justify-self-center">
+          <button
+            className="menu-btn md:hidden"
+            onClick={null}
+          >
+            <span class="material-symbols-rounded">
+              menu
+            </span>
+          </button>
+
+          Navbar
+        </div>
+
+        <a
+          href="#contact"
+          className=""
+        >
+          Contact Me
+        </a>
+      </div>
+    </header>
   )
 }
 
