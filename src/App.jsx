c651523de177ef0b64f84afc2c5c08c79a879ed3
Michael Stiven Saa Motta <michaelsaams20@gmail.com>
2025-01-13 19:20:52 -0500

    star home with images for deafault the course

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,16 @@
  */
 
 import { Header } from "./components/Header"
+import { Hero } from "./components/Hero"
 export const App = () =>{
 
 
     return (
         <>
             <Header />
+            <main>
+                <Hero />
+            </main>
         </>
     )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,59 @@
+/**
+ * @copyright 2025 MichaelSaa_
+ * @license Apache-2.0              
+ */
+
+import React from "react";
+
+export const Hero = () => {
+    return (
+        <senction
+            id="home"
+            className="">
+
+            <div className="">
+                <div>
+                   <div className="">
+                        <figure className="">
+                            <img
+                                src="/images/avatar-1.png"
+                                width={40}
+                                height={40}
+                                alt="portrait"
+                                className
+                            />
+                        </figure>
+                        <div className="">
+                            <span className="">
+                                <span className=""></span>
+
+                            </span>
+                            Available for work
+                        </div>
+                    </div>
+                    <h2 className="">
+                        Building Scalable Modern Websites for the Future
+                    </h2>
+                    <div className="">
+                        ButtonPrimary 
+
+                        ButtonOutline 
+                    </div>
+                </div>
+
+
+                <div className="">
+                    <figure className="">
+                        <img
+                         src="/images/hero-banner.png"
+                         width={400} 
+                         alt=""
+                         className=""
+                          />
+                    </figure>
+                </div>
+            </div>
+
+        </senction>
+    )
+}
